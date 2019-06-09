b6ff747bdce88326ba94ccef1bf0f601a7d0d5df
Omar <omarh.ismail1@gmail.com>
2019-06-09 17:46:14 +0200

    Added Splash Screen

--- android/app/src/main/java/com/warhit/yala_mazad/MainActivity.java
+++ android/app/src/main/java/com/warhit/yala_mazad/MainActivity.java
@@ -3,11 +3,24 @@ package com.warhit.yala_mazad;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    //make transparent status bar
+    getWindow().setStatusBarColor(0x00000000);
     GeneratedPluginRegistrant.registerWith(this);
+    //Remove full screen flag after load
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
   }
 }
